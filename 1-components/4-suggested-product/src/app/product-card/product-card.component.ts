07f7333e6b56676fc56aae02cb6783a06a5aeb3b
Eshkinin Ilya <I.Eshkinin@treolan.ru>
2020-09-10 00:24:52 +0300

    4. suggested-product

--- 1-components/4-suggested-product/src/app/product-card/product-card.component.ts
+++ 1-components/4-suggested-product/src/app/product-card/product-card.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { IProduct } from 'shared/mocks/1-components/product';
 
 @Component({
   selector: 'ngx-shop-product-card',
   templateUrl: './product-card.component.html',
 })
 export class ProductCardComponent {
+  @Output()
+  public goToProduct = new EventEmitter();
 
+  @Input()
+  public product = {} as IProduct;
+
+  public redirectTo(): void{
+    this.goToProduct.emit();
+  }
 }
