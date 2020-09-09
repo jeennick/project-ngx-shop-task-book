cb7b3eaa6f9aa546e2f21852e9464b9f12f87c63
Eshkinin Ilya <I.Eshkinin@treolan.ru>
2020-09-10 00:16:32 +0300

    3. category-product-card

--- 1-components/3-category-product-card/src/app/app.component.ts
+++ 1-components/3-category-product-card/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { oneProduct } from '../../../../shared/mocks/1-components/product';
 export class AppComponent {
   public product = oneProduct;
   public terminalMessage: string;
-  public title = '1. Интерполяция и связывание (Карточка товара cтраницы подкатигории)';
+  public title = '1. Интерполяция и связывание (Карточка товара cтраницы подкатегории)';
   public addToCart() {
     this.terminalMessage = 'товар добавлен в карзину';
   }
--- 1-components/3-category-product-card/src/app/product-card/product-card.component.ts
+++ 1-components/3-category-product-card/src/app/product-card/product-card.component.ts
@@ -1,7 +1,11 @@
 import {
   Component,
   ChangeDetectionStrategy,
+  Input,
+  Output,
+  EventEmitter,
 } from '@angular/core';
+import { IProduct } from 'shared/mocks/1-components/product';
 
 @Component({
   selector: 'ngx-shop-content-product',
@@ -9,6 +13,21 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CategoryProductComponent {
+  @Input()
+  public product = {} as IProduct;
 
+  @Output()
+  public addToCart = new EventEmitter();
+
+  @Output()
+  public goToProduct = new EventEmitter();
+
+  public addToBasket(): void{
+    this.addToCart.emit();
+  }
+
+  public redirectTo(): void{
+    this.goToProduct.emit();
+  }
 
 }
