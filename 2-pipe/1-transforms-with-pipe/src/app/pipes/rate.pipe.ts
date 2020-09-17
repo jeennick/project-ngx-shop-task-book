101ffc1b8460c6ccc7379d4b995ef7f6366307f3
Eshkinin Ilya <I.Eshkinin@treolan.ru>
2020-09-17 19:00:54 +0300

    2.pipes

--- 2-pipe/1-transforms-with-pipe/src/app/pipes/rate.pipe.ts
+++ 2-pipe/1-transforms-with-pipe/src/app/pipes/rate.pipe.ts
@@ -6,6 +6,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 // @ts-ignore
 export class RatePipe implements PipeTransform {
   public transform(value: number): number | undefined {
-    return 0;
+    if (value === 0)
+    {
+      return value;
+    }
+    const frac = value % 1;
+    const num = value - frac;
+    if (frac < 0.25){
+      return num ;
+    }
+
+    if (frac < 0.75){
+      return num + 0.5;
+    }
+
+    return num + 1;
   }
 }
