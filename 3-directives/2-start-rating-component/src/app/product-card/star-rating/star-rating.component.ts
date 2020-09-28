acff7953b3567544588dbda45c3ac728ed6e2574
Ilya Eshkinin <jeennick@gmail.com>
2020-09-28 17:06:56 +0300

    3-directives task 2

--- 3-directives/2-start-rating-component/src/app/product-card/star-rating/star-rating.component.ts
+++ 3-directives/2-start-rating-component/src/app/product-card/star-rating/star-rating.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ngx-shop-star-rating',
   templateUrl: './star-rating.component.html',
 })
 export class StarRatingComponent {
+  @Input()
+  public feedbackRate = 0;
 
+  public maxRate = 5;
+
+  public stars: number[] = [0, 1, 2, 3, 4];
+
+  public highlight(index: number): boolean{
+    return index < this.feedbackRate;
+  }
 }
