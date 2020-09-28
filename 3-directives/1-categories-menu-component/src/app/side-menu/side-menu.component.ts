341854e645d87a1928a04892bedea0cc76127fcb
Ilya Eshkinin <jeennick@gmail.com>
2020-09-28 16:31:03 +0300

    3-directives 1

--- 3-directives/1-categories-menu-component/src/app/side-menu/side-menu.component.ts
+++ 3-directives/1-categories-menu-component/src/app/side-menu/side-menu.component.ts
@@ -1,7 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { ICategory } from '../../../../../shared/mocks/3-directives/categories';
 
 @Component({
   selector: 'ngx-shop-side-menu',
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent {}
+export class SideMenuComponent {
+  @Input()
+  public categories: ICategory[] = [];
+
+  @Output()
+  public categoryChange = new EventEmitter();
+
+  public currentName: string = null;
+
+  public hover(name: string): void{
+    this.currentName = name;
+  }
+
+  public mouseLeave(): void{
+    this.currentName = null;
+  }
+
+  public redirectTo(newSubCategory: ICategory): void{
+    this.categoryChange.emit(newSubCategory.name);
+  }
+}
