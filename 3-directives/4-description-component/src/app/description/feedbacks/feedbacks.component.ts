f7a7d3014c8a2233b86d3e7c0379e7681bb37c76
Ilya Eshkinin <jeennick@gmail.com>
2020-10-01 16:53:41 +0300

    3-directives 4-description-component

--- 3-directives/4-description-component/src/app/description/feedbacks/feedbacks.component.ts
+++ 3-directives/4-description-component/src/app/description/feedbacks/feedbacks.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { IFeedback } from './../../../../../../shared/interfaces/product.interface';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'ngx-shop-feedbacks',
   templateUrl: './feedbacks.component.html',
   styleUrls: ['./feedbacks.component.sass'],
 })
-export class FeedbacksComponent {}
+export class FeedbacksComponent {
+
+  @Input()
+  public feedbacks: IFeedback[] = [];
+
+  @Output()
+  public addFeedbackEvent = new EventEmitter<string>();
+
+
+
+  public addNewFeedback(): void{
+    this.addFeedbackEvent.emit('отзыв добавлен');
+  }
+
+}
